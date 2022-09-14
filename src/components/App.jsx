896b8a01ab606d7e24851ddb50cb7f744dac079f
Ivanna Honcharuk <Ivannatsyganjuk@gmail.com>
2022-09-15 00:44:42 +0200

    add localstorage to phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,101 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { Phonebook } from "./Phonebook/Phonebook";
+import { GlobalStyle } from './GlobalStyle';
+import { Section } from './Section/Section';
+import { ContactsList } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
+
+export class App extends Component {
+ state = {
+  contacts: [],
+  filter: ''
+  }
+
+  getListFromLocalStorage = () => {
+    localStorage.getItem('contacts') &&
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts')),
+      });
+  };
+  
+  componentDidMount() {
+    try {
+      this.getListFromLocalStorage();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  isContactChange = () => {
+    return (
+      localStorage.getItem('contacts') &&
+      JSON.parse(localStorage.getItem('contacts')) !== this.state.contacts);
+  };
+
+  componentDidUpdate() {
+    try {
+      if (this.isContactChange) {
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleNewContact = newContact => {
+    if (!this.hasDuplicates(newContact.name)) {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newContact],
+        };
+      });
+    } else {
+      alert(`${newContact.name} is already in contacts.`);
+    }
+  };
+
+  hasDuplicates(duplicate) {
+    return this.state.contacts.find(({ name }) => name === duplicate);
+  }
+
+   getFilteredContacts = () => {
+    const filterValue = this.state.filter.toLowerCase();
+
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filterValue)
+    );
+  };
+
+  onInputChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  deleteContact = contactIdToDelete => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        ({ id }) => id !== contactIdToDelete
+      ),
+    });
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <>
+        <GlobalStyle />
+        <Section title="Phonebook">
+          <Phonebook handleNewContact={this.handleNewContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter onInputChange={this.onInputChange} />
+          <ContactsList
+            contacts={filteredContacts}
+            deleteContact={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
+  }
 };
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Label, FormBox } from './Phonebook.styled';
+
+
+export class Phonebook extends Component {
+  static propTypes = {
+    handleNewContact: PropTypes.func.isRequired,
+  };
+
+  state = {
+    name: '',
+    number: '',
+  };
+
+  onInputChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: evt.target.elements.name.value,
+      number: evt.target.elements.number.value,
+    };
+
+    this.props.handleNewContact(newContact);
+    this.resetInputs();
+  };
+
+  resetInputs() {
+    this.setState({ name: '', number: '' });
+  }
+
+  render() {
+    return (
+        <FormBox onSubmit={this.onFormSubmit} autoComplete="off">
+          <Label>
+            <span>Name</span>
+            <input
+              onChange={this.onInputChange}
+              value={this.state.name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </Label>
+          <Label>
+            <span>Number</span>
+            <input
+              onChange={this.onInputChange}
+              value={this.state.number}
+              name="number"
+              type="tel"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </Label>
+          <button type="submit">Add contact</button>
+        </FormBox>
+    );
+  }
+}
